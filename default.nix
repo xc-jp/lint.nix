7c42f8522a6324f624ea5381f356f93ad926b374
Jonas Carpay <jonascarpay@gmail.com>
2021-10-23 16:20:17 +0900

    Move nix to nix-old
    This is to make sure that the new nix tooling only uses parts of the old
    tooling that we explicitly move over. This directory is removed at the
    end.

--- default.nix
+++
@@ -1,7 +0,0 @@
-{
-  # Remove all null values in an attribute set
-  removeNulls = obj:
-    let nulls = builtins.filter (name: builtins.getAttr name obj == null)
-      (builtins.attrNames obj);
-    in builtins.removeAttrs obj nulls;
-}
